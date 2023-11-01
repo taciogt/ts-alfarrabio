184486d2376157a984fb8ba8b800c24df7ad1078
Tácio Tavares <tacio@latitud.com>
2023-11-01 19:36:33 -0300

    tests for linked list to string method

--- src/linked-list/linked-list.test.ts
+++ src/linked-list/linked-list.test.ts
@@ -1,8 +1,20 @@
 import {describe, expect, test} from '@jest/globals';
-import { hello } from './linked-list'
+import { hello, LinkedList } from './linked-list'
 
 describe('hello world of test', () => {
   test('tests if default return is hello', () => {
     expect(hello()).toBe('hello');
+  });
+});
+
+describe('Linked List toString', () => {
+  test('list with a single element', () => {
+    const list = new LinkedList(1)
+    expect(list.toString()).toBe('LinkedList<1>');
+  });
+
+  test('list with two elements', () => {
+    const list = new LinkedList(1, 2)
+    expect(list.toString()).toBe('LinkedList<1>->LinkedList<2>');
   });
 });
--- src/linked-list/linked-list.ts
+++ src/linked-list/linked-list.ts
@@ -1,1 +1,18 @@
 export const hello = () => 'hello'
+
+export class LinkedList<T> {
+  private readonly value: T
+  private readonly next: LinkedList<T> | null = null
+
+  constructor(...items: [T, ...T[]]) {
+    this.value = items[0]
+    if (items.length > 1) {
+      this.next = new LinkedList<T>(...[items[1], ...items.slice(2)])
+    }
+  }
+
+  toString(): string {
+    const nextString = this.next ? `->${this.next.toString()}` : ''
+    return `LinkedList<${this.value}>${nextString}`
+  }
+}
